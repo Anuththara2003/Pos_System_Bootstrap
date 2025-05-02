363bee632621862d09019597a285d8aaf2c302be
Sandaru Anuththara <sandaruperera108@gmail.com>
2025-05-02 23:11:45 +0530

    done update customer and item

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -27,6 +27,121 @@ $('#save-customer').on ("click",function() {
 
 });
 
+$('#customer_update_btn').on("click", function () {
+    let customer_id = $("#id").val().trim();
+    let name = $("#name").val().trim();
+    let address = $("#addres").val().trim();
+    let contact = $("#contct").val().trim();
+
+    if (!customer_id) {
+        Swal.fire({
+            title: 'Invalid Id',
+            text: 'Please Enter Valid Id',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!name) {
+        Swal.fire({
+            title: 'Invalid Name',
+            text: 'Please Enter Valid Name',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!address) {
+        Swal.fire({
+            title: 'Invalid Address',
+            text: 'Please Enter Valid Address',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!contact) {
+        Swal.fire({
+            title: 'Invalid Contact',
+            text: 'Please Enter Valid Contact',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else {
+        let customer = customer_db.find((item) => item.customer_id === customer_id);
+
+        if (!customer) {
+            Swal.fire({
+                title: 'Not Found',
+                text: 'Customer not found in database.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+            return;
+        }
+
+        customer.name = name;
+        customer.address = address;
+        customer.contact = contact;
+        loadTable();
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, update it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Updated!",
+                    text: "Customer has been updated.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+});
+
+
+
+$("#customer-tbody").on('click', 'tr', function(){
+
+    let idx = $(this).index();
+    console.log(idx);
+    let obj = customer_db[idx];
+    console.log(obj);
+
+    let id = obj.customer_id;
+    let name = obj.name;
+    let address = obj.address;
+    let contact = obj.contact
+
+
+    $('#name').val(name);
+    $('#addres').val(address);
+    $('#contct').val(contact);
+    $('#id').val(id);
+
+});
+
+
 function  loadTable(){
     $('#customer-tbody').empty();
     customer_db.map((item, index) => {
--- Controller/ItemController.js
+++ Controller/ItemController.js
@@ -23,6 +23,124 @@ $('#save_item').on ("click",function() {
 
 });
 
+$("#item-tbody").on('click', 'tr', function(){
+
+    let idx = $(this).index();
+    console.log(idx);
+    let obj = item_db[idx];
+    console.log(obj);
+
+    let id = obj.id;
+    let name = obj.name;
+    let price = obj.price;
+    let quantity = obj.quantity
+
+    $('#item_id').val(id);
+    $('#names').val(name);
+    $('#qty').val(quantity);
+    $('#price').val(price);
+
+
+    console.log(id);
+
+});
+
+
+$('#item_update_button').on("click", function () {
+    let id = $("#item_id").val().trim();
+    console.log(id);
+    let name = $("#names").val().trim();
+    let price = $("#price").val().trim();
+    let quantity = $("#qty").val().trim();
+
+    if (!id) {
+        Swal.fire({
+            title: 'Invalid Id',
+            text: 'Please Enter Valid Id',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!name) {
+        Swal.fire({
+            title: 'Invalid Name',
+            text: 'Please Enter Valid Name',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!price) {
+        Swal.fire({
+            title: 'Invalid Price',
+            text: 'Please Enter Valid Price',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else if (!quantity) {
+        Swal.fire({
+            title: 'Invalid Quantity',
+            text: 'Please Enter Valid Quantity',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'OK',
+            customClass: {
+                title: 'error-title',
+                content: 'error-content'
+            }
+        });
+    } else {
+        let item = item_db.find((item) => item.id === id);
+
+        if (!item) {
+            swal.fire({
+                title: 'Not Found',
+                text: 'Item not found in database.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+            return;
+        }
+
+        item.name = name;
+        item.price = price;
+        item.quantity = quantity;
+        loadTable();
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, update it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Updated!",
+                    text: "Your item has been updated.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+});
+
+
+
 
 function loadTable(){
     $('#item-tbody').empty();
